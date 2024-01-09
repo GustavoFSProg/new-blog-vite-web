af080d60e20dffb1345909067c8707961e21e91e
GusFSProg <gustavo.prog40@gmail.com>
2024-01-08 21:31:58 -0300

    navbar-responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,9 @@
 import styled from 'styled-components'
 import Drawer from '../drawer'
-// import Drawer from '@mui/material/Drawer'
-// import SwipeableTemporaryDrawer from '../drawer'
-// import TemporaryDrawer from '../Drawer'
 
 const Container = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
   width: 100vw;
   height: auto;
@@ -24,15 +21,31 @@ const Container = styled.div`
 
 const Blogger = styled.div`
   display: flex;
+  width: 100vw;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  /* background: green; */
+  /* margin-left: -40px; */
+
+  @media screen and (max-width: 850px) {
+    width: 80%;
+    /* margin-left: -10px; */
+  }
+`
+
+const TitleContainer = styled.div`
+  display: flex;
   width: 82%;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   /* background: green; */
+  /* margin-left: -40px; */
 
   @media screen and (max-width: 850px) {
     width: 80%;
-    margin-left: -10px;
+    /* margin-left: -10px; */
   }
 `
 
@@ -49,30 +62,35 @@ const Span = styled.div`
 `
 
 const Botao = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* width: 100vw; */
+  /* height: auto; */
   @media screen and (max-width: 850px) {
-    margin-left: -25px;
+    margin-right: 50px;
   }
 `
 
 export default function Navbar() {
   return (
     <Container>
-      <Botao>
-        <Drawer />
-      </Botao>
       <Blogger>
-        <Span>BLOGGER</Span>
-        <span
-          style={{
-            fontSize: '14px',
-            letterSpacing: '10px',
-            fontStyle: 'italic',
-          }}
-        >
-          Design
-        </span>
-        {/* <span style={{ fontSize: '20px' }}>BLOGGER</span>
-        <span style={{ fontSize: '16spanx', fontStyle: 'italic' }}>Design</span> */}
+        <Botao>
+          <Drawer />
+        </Botao>
+        <TitleContainer>
+          <Span>BLOGGER</Span>
+          <span
+            style={{
+              fontSize: '14px',
+              letterSpacing: '10px',
+              fontStyle: 'italic',
+            }}
+          >
+            Design
+          </span>
+        </TitleContainer>
       </Blogger>
     </Container>
   )
