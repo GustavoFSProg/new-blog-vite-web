1a55f6a996a087e236607fd4094e6ec7d857b46c
GusFSProg <gustavo.prog40@gmail.com>
2024-01-22 09:06:58 -0300

    widthaa

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -54,7 +54,7 @@ const TotalContainer = styled.div`
   @media screen and (max-width: 820px) {
     flex-direction: column;
     padding-bottom: 60px;
-    width: 83vw;
+    width: 82vw;
   }
 `
 
@@ -184,7 +184,7 @@ const ContainerTexto = styled.div`
 
   @media screen and (max-width: 820px) {
     width: 100%;
-    margin-left: 21px;
+    margin-left: 5px;
     /* padding-right: 20px; */
     font-size: 17px;
   }
